5d5b59af884aed9c0e7647d095d8b180fe651ad3
bahoo <jon.c.culver@gmail.com>
2017-02-01 18:29:42 -0800

    Call it a v1

--- map/views.py
+++ map/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import City
 
-# Create your views here.
+
+
+class MapView(TemplateView):
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(MapView, self).get_context_data(**kwargs)
+        cities = []
+        for city in City.objects.select_related('state').defer('state__geom').all():
+            city.geojson = city.geom.simplify(0.00001).geojson
+            cities.append(city)
+        context['cities'] = cities
+        return context
