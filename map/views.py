505bdeb687355693ad9d52420d51284fda2036a5
bahoo <jon.c.culver@gmail.com>
2017-02-02 08:06:40 -0800

    Gzip what, gzip what

--- map/views.py
+++ map/views.py
@@ -10,7 +10,7 @@ class MapView(TemplateView):
         context = super(MapView, self).get_context_data(**kwargs)
         cities = []
         for city in City.objects.select_related('state').defer('state__geom').all():
-            city.geojson = city.geom.simplify(0.00001).geojson
+            city.geojson = city.geom.simplify(0.0015, preserve_topology=True).geojson
             cities.append(city)
         context['cities'] = cities
         return context
