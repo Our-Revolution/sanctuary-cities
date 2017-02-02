7812a0f762bf290d0512ed12c294a89520c5c9e0
bahoo <jon.c.culver@gmail.com>
2017-02-02 15:26:05 -0800

    Simplifying Python a bit.

--- map/views.py
+++ map/views.py
@@ -10,7 +10,9 @@ class MapView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(MapView, self).get_context_data(**kwargs)
-        simplified = RawSQL('ST_Transform(ST_Simplify(map_city.geom, 0.0005), 4326)', ())
+        simplified = RawSQL('ST_Simplify(map_city.geom, 0.0005)', ())
         simplified.srid = True
-        context['cities'] = City.objects.select_related('state').defer('state__geom').annotate(simplified=simplified).annotate(geojson=AsGeoJSON('simplified', precision=3)).all()
+        context['cities'] = City.objects.select_related('state').defer('state__geom') \
+                                    .annotate(simplified=simplified,
+                                            geojson=AsGeoJSON('simplified', precision=3)).all()
         return context
