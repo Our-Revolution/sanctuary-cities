65fcc7f62b4c261c03b94489f911c65a72d0edf7
bahoo <jon.c.culver@gmail.com>
2017-02-07 10:49:03 -0800

    Details!

---
+++ map/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from map.views import *
+
+urlpatterns = [    
+    url(r'^$', MapView.as_view(), name="index"),
+    url(r'^(?P<slug>[\w\-]+)$', MapDetailView.as_view(), name="detail"),
+]
--- sanctuarycities/urls.py
+++ sanctuarycities/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from map.views import *
+
 
 urlpatterns = [
-    url(r'^$', MapView.as_view()),
+    url(r'', include('map.urls', namespace="map")),
     url(r'^admin/', admin.site.urls),
-    url(r'^(?P<slug>[\w\-]+)$', MapDetailView.as_view()),
 ]
