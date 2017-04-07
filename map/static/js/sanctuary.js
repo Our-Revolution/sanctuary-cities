c1e923b67c75bd4e04dde0f766c5cbc90275e7e4
Chris Mabry <cjmab28@gmail.com>
2017-04-07 00:06:46 -0500

    Show policy summary in the sidebar

--- map/static/js/sanctuary.js
+++ map/static/js/sanctuary.js
@@ -147,26 +147,172 @@ var sanctuary = (function($) {
     
     for (var i=0; i<layers.length; i++) {
     
-      var description, name = "";
+      var description = "", name = "", policy_summary = {'good': [], 'bad': []};
       
-      if(layers[i].properties.model == "map.county") {
-        name = layers[i].properties.name;
+      name = layers[i].properties.name;
+            
+      if (layers[i].properties.model == "map.county") {
+        if (layers[i].properties.jails_honor_ice_detainers == 'yes-by-law' || layers[i].properties.jails_honor_ice_detainers == 'yes-in-practice') {
+          policy_summary.good.push('Jails do not honor ICE detainers');
+        } else if (layers[i].properties.jails_honor_ice_detainers == 'unlimited') {
+          policy_summary.bad.push('Jails honor ICE detainers');
+        }
         
-        if (!layers[i].properties.jails_honor_ice_detainers_short_answer.includes('N/A')) {
-          description = layers[i].properties.jails_honor_ice_detainers_short_answer;
+        if(layers[i].properties.jails_prohibit_inquiries == true) {
+          policy_summary.good.push('Jails prohibit inquiries into immigration status and/or place of birth');
+        } else if (layers[i].properties.jails_prohibit_inquiries == false) {
+          policy_summary.bad.push('Jails do not prohibit inquiries into immigration status and/or place of birth');
+        }
+        
+        if(layers[i].properties.ice_contracts == true) {
+          policy_summary.bad.push('Participates in programs like 287(g)');
+        } else if (layers[i].properties.ice_contracts == false) {
+          policy_summary.good.push('Does not participate in programs like 287(g)');
+        }
+        
+        if(layers[i].properties.isga == true) {
+          policy_summary.bad.push('Jails have an IGSA');
+        } else if (layers[i].properties.isga == false) {
+          policy_summary.good.push('Jails don\'t have an IGSA');
+        }
+        
+        if(layers[i].properties.preventing_policies == true) {
+          policy_summary.good.push('Prevents local police from acting as immigration agents');
+        } else if (layers[i].properties.preventing_policies == false) {
+          policy_summary.bad.push('Does not prevent local police from acting as immigration agents');
+        }
+        
+        if(layers[i].other_policies_and_services) {
+          policy_summary.good.push('Has implemented other policies more welcoming to immigrants, people of color, the LGBTQ community or religious minorities');
+        }
+        
+        if(layers[i].properties.permitting_policies == true) {
+          policy_summary.bad.push('Permits local police from acting as immigration agents');
+        } else if (layers[i].properties.permitting_policies == false) {
+          policy_summary.good.push('Does not permit local police from acting as immigration agents');
+        }
+          
+      } else if (layers[i].properties.model == "map.city") {        
+        
+        if (layers[i].properties.limited_ice_cooperation == 'yes-by-law' || layers[i].properties.limited_ice_cooperation == 'yes-in-practice') {
+          policy_summary.good.push('Limited cooperation with ICE');
+        } else if (layers[i].properties.limited_ice_cooperation == 'unlimited') {
+          policy_summary.bad.push('Cooperates with ICE');
+        }
+        
+        if (layers[i].properties.participate_287g_program == true) {
+          policy_summary.bad.push('Participates in the 287(g) program');
+        } else if (layers[i].properties.participate_287g_program == false) {
+          policy_summary.good.push('Does not participate in the 287(g) program');
+        }
+        
+        if(layers[i].properties.isga == true) {
+          policy_summary.bad.push('City jails have an IGSA');
+        } else if (layers[i].properties.isga == false) {
+          policy_summary.good.push('City jails do not have an IGSA');
+        }
+        
+        if(layers[i].properties.provide_legal_representation == true) {
+          policy_summary.good.push('Provides undocumented immigrants with legal representation in court');
+        } else if (layers[i].properties.provide_legal_representation == false) {
+          policy_summary.bad.push('Does not provide undocumented immigrants with legal representation in court');
+        }
+        
+        if(layers[i].properties.city_services == true) {
+          policy_summary.bad.push('Asks about immigration status when applying or accessing city services');
+        } else if (layers[i].properties.city_services == false) {
+          policy_summary.good.push('Does not ask about immigration status when applying or accessing city services');
+        }
+        
+        if(layers[i].properties.drivers_license == true) {
+          policy_summary.good.push('Undocumented immigrants can get driver\'s licenses');
+        } else if (layers[i].properties.drivers_license == false) {
+          policy_summary.bad.push('Undocumented immigrants cannot get driver\'s licenses');
+        }
+        
+        if(layers[i].properties.separate_form_of_id == true) {
+          policy_summary.good.push('Provides form of ID if driver\'s licenses aren\'t available');
+        } else if (layers[i].properties.separate_form_of_id == false) {
+          policy_summary.bad.push('Does not provide form of ID if driver\'s licenses aren\'t available');
+        }
+        
+        if(layers[i].properties.police_use_body_cameras == true) {
+          policy_summary.good.push('City police use body cameras');
+        } else if (layers[i].properties.police_use_body_cameras == false) {
+          policy_summary.bad.push('City police don\'t use body cameras');
+        }
+        
+        if(layers[i].properties.other_policies_and_services) {
+          policy_summary.good.push('Has other policies more welcoming to immigrants, people of color, the LGBTQ community and religious minorities');
+        }
+        
+      } else if (layers[i].properties.model == "map.state") {
+        if (layers[i].properties.limited_ice_cooperation == 'yes-by-law' || layers[i].properties.limited_ice_cooperation == 'yes-in-practice') {
+          policy_summary.good.push('Limited cooperation with ICE');
+        } else if (layers[i].properties.limited_ice_cooperation == 'unlimited') {
+          policy_summary.bad.push('Cooperates with ICE');
+        }
+        
+        if(layers[i].properties.ice_contracts == true) {
+          policy_summary.bad.push('Has 287(g) contracts or similar');
+        } else if (layers[i].properties.ice_contracts == false) {
+          policy_summary.good.push('Does not have 287(g) contracts or similar');
+        }
+        
+        if(layers[i].properties.isga == true) {
+          policy_summary.bad.push('State jails have an IGSA');
+        } else if (layers[i].properties.isga == false) {
+          policy_summary.good.push('State jails don\'t have an IGSA');
+        }
+        
+        if(layers[i].properties.provide_legal_representation == true) {
+          policy_summary.good.push('Provides undocumented immigrants with legal representation in court');
+        } else if (layers[i].properties.provide_legal_representation == false) {
+          policy_summary.bad.push('Does not provide undocumented immigrants with legal representation in court');
+        }
+        
+        if(layers[i].properties.drivers_license == true) {
+          policy_summary.good.push('Undocumented immigrants can get driver\'s licenses');
+        } else if (layers[i].properties.drivers_license == false) {
+          policy_summary.bad.push('Undocumented immigrants cannot get driver\'s licenses');
+        }
+        
+        if(layers[i].properties.in_state_tuition == true) {
+          policy_summary.good.push('Undocumented immigrants can get in-state tuition at public colleges');
+        } else if (layers[i].properties.in_state_tuition == false) {
+          policy_summary.bad.push('Undocumented immigrants cannot get in-state tuition at public colleges');
+        }
+        
+        if(layers[i].properties.barrier == true) {
+          policy_summary.bad.push('Immigration status acts as a barrier in accessing state services');
+        } else if (layers[i].properties.barrier == false) {
+          policy_summary.good.push('Immigration status does not act as a barrier in accessing state services');
+        }
+        
+        if(layers[i].properties.policies_against_profiling == true) {
+          policy_summary.good.push('Has policies against profiling');
+        } else if (layers[i].properties.policies_against_profiling == false) {
+          policy_summary.bad.push('Does not have policies against profiling');
+        }
+        
+        if(layers[i].properties.other_policies_and_services) {
+          policy_summary.good.push('Has other policies more welcoming to immigrants, people of color, the LGBTQ community and religious minorities');
+        }
+      }
+      
+      if (policy_summary.good.length > 0 || policy_summary.bad.length > 0) {
+        description = '<h4 class="mt0 fw7">Policy Summary</h4>';
+        for (var j = 0; j< policy_summary.good.length; j++) {
+          description += '<li class="policy--good">' + policy_summary.good[j] + '</li>';
+        }
+        
+        for (var j = 0; j< policy_summary.bad.length; j++) {
+          description += '<li class="policy--bad">' + policy_summary.bad[j] + '</li>';
         }
       } else {
-        name = layers[i].properties.name;
-        
-        if (!layers[i].properties.limited_ice_cooperation_short_answer.includes('N/A')) {
-          description = layers[i].properties.limited_ice_cooperation_short_answer;
-        }
-      }
-      
-      if (!description) {
         description = "Click the button below to learn more about " + layers[i].properties.name + "'s policies and how you can get involved."
       }
-                
+                      
       layers[i].options.oldColor = layers[i].options.color;
       
       $('.app-info__status').append('\
@@ -175,9 +321,11 @@ var sanctuary = (function($) {
             <h4 class="component__name">' + name + '</h4>\
           </div>\
           <div class="component__info">\
-            <p class="component__description">\
+            <div class="component__description">\
+              <ul class="policy-list">\
               ' + description + '\
-            </p>\
+              </ul>\
+            </div>\
             <a href="/'+ layers[i].properties.slug +'" class="component__cta btn btn-block btn-success btn-uppercase">Get Involved</a>\
           </div> \
         </div>\
